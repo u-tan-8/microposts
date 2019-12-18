8a53cf83ac0cbbe7eb45ff97ef61b69030abf9b4
yuuji.maeda <utan.ym@gmail.com>
2019-12-18 05:12:39 +0000

    post

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::orderBy('id', 'desc')->paginate(10);
+
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    }
+    
+    public function show($id)
+    {
+        $user = User::find($id);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'microposts' => $microposts,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.show', $data);
+    }
+    
+}
+
